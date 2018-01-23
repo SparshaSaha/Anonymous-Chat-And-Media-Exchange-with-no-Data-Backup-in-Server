dbc0f12f9de9b515463c4a51bbacb15814c00778
SparshaSaha <saha.sparsha@gmail.com>
2018-01-24 02:50:18 +0530

    register completed

--- Server/Routes.js
+++ Server/Routes.js
@@ -1,5 +1,6 @@
 var User=require("./Models/User");
 var socketio = require('socket.io');
+var http = require('http');
 
 module.exports= function(mongo){
 console.log("here");
@@ -14,7 +15,23 @@ var server = http.createServer(function(req, res) {
 });
 
 socketio.listen(server).on('connection', function (socket) {
-  
+
+  socket.on('register',function(mail){
+    User.find({email:mail},function(err,resp){
+      if(err){
+
+      }
+      else {
+          if(resp.length==0)
+          socket.emit('register_reply',"not an user");
+          else {
+            socket.emit('register_reply',"registered");
+          }
+      }
+    });
+  });
+
+
 });
 
 }
