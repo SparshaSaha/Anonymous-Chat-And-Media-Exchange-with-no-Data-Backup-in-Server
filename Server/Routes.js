e54258ece966e23cb0da337929f0ad8eca138c2b
SparshaSaha <saha.sparsha@gmail.com>
2018-01-24 02:44:53 +0530

    Register progressed

--- Server/Routes.js
+++ Server/Routes.js
@@ -3,4 +3,18 @@ var socketio = require('socket.io');
 
 module.exports= function(mongo){
 console.log("here");
+const port=process.env.PORT || 8080;
+
+onlinemap=new Map();
+revonlinemap=new Map();
+
+var server = http.createServer(function(req, res) {
+}).listen(port, function() {
+    console.log('Listening at: http://localhost:'+port);
+});
+
+socketio.listen(server).on('connection', function (socket) {
+  
+});
+
 }
